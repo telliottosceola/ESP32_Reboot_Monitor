cb0842c864d5236e10a8663d1e886eb7cf30b4db
Travis Elliott <travis@Production-Mac-mini.local>
2019-03-26 16:06:43 -0500

    update

--- index.js
+++ index.js
@@ -2,36 +2,36 @@ const sp = require("serialport");
 const sensor = require('./sensor_spoof.js');
 const port = new sp("/dev/cu.SLAB_USBtoUART", { baudRate: 115200 });
 var transCount;
-// sensor();
-port.on("open", function(err){
-  if(err){
-    console.log(err);
-  }else{
-    port.set({rts:false, dtr:false},function(err){
-      if(err){
-        console.log(err);
-      }else{
-		process.stdout.write(`Waiting for Transmissions`);
-        sensor();
-      }
-    });
-  }
-});
-
-
-port.on('data', function(data){
-  data = data.toString();
-
-  if(data.indexOf("Transmission count:") > -1){
-    transCount = data.split(": ").pop()*1;
-	process.stdout.clearLine();
-	process.stdout.cursorTo(0);
-	process.stdout.write(`running after ${transCount} transmissions`);
-  }
-
-  if(data.indexOf("WiFi connected to Travis-WiFi") > -1){
-    console.log("\n=================================");
-    console.log(`rebooted after ${transCount} transmissions`);
-    console.log("=================================");
-  }
-});
+sensor();
+// port.on("open", function(err){
+//   if(err){
+//     console.log(err);
+//   }else{
+//     port.set({rts:false, dtr:false},function(err){
+//       if(err){
+//         console.log(err);
+//       }else{
+// 		process.stdout.write(`Waiting for Transmissions`);
+//         sensor();
+//       }
+//     });
+//   }
+// });
+//
+//
+// port.on('data', function(data){
+//   data = data.toString();
+//
+//   if(data.indexOf("Transmission count:") > -1){
+//     transCount = data.split(": ").pop()*1;
+// 	process.stdout.clearLine();
+// 	process.stdout.cursorTo(0);
+// 	process.stdout.write(`running after ${transCount} transmissions`);
+//   }
+//
+//   if(data.indexOf("WiFi connected to Travis-WiFi") > -1){
+//     console.log("\n=================================");
+//     console.log(`rebooted after ${transCount} transmissions`);
+//     console.log("=================================");
+//   }
+// });
